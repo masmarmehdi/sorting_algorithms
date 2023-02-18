26305595f1a768189e82b441d7ec467ab0902943
Mehdi Masmar <masmar507@gmail.com>
2023-02-18 11:16:42 -0800

    Adding quick sort to header file

--- sort.h
+++ sort.h
@@ -22,4 +22,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void print_list(const listint_t *list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
